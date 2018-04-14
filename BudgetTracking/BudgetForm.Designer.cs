177f5e23c28a1a2f71ec4be94fb92bb6e9a0f435
Jeremy Farmer <you.icaruspsy@gmail.com>
2018-04-14 13:28:09 -0400

    Add Basic Labels

--- BudgetTracking/BudgetForm.Designer.cs
+++ BudgetTracking/BudgetForm.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelSingleItem = new System.Windows.Forms.Label();
+            this.labelGroup = new System.Windows.Forms.Label();
+            this.labelBudgetInfo = new System.Windows.Forms.Label();
+            this.labelAdd = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // labelSingleItem
+            // 
+            this.labelSingleItem.AutoSize = true;
+            this.labelSingleItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSingleItem.Location = new System.Drawing.Point(13, 25);
+            this.labelSingleItem.Name = "labelSingleItem";
+            this.labelSingleItem.Size = new System.Drawing.Size(233, 24);
+            this.labelSingleItem.TabIndex = 0;
+            this.labelSingleItem.Text = "Largest Item This Month";
+            // 
+            // labelGroup
+            // 
+            this.labelGroup.AutoSize = true;
+            this.labelGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGroup.Location = new System.Drawing.Point(436, 25);
+            this.labelGroup.Name = "labelGroup";
+            this.labelGroup.Size = new System.Drawing.Size(252, 24);
+            this.labelGroup.TabIndex = 1;
+            this.labelGroup.Text = "Largest Group This Month";
+            // 
+            // labelBudgetInfo
+            // 
+            this.labelBudgetInfo.AutoSize = true;
+            this.labelBudgetInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBudgetInfo.Location = new System.Drawing.Point(917, 25);
+            this.labelBudgetInfo.Name = "labelBudgetInfo";
+            this.labelBudgetInfo.Size = new System.Drawing.Size(190, 24);
+            this.labelBudgetInfo.TabIndex = 2;
+            this.labelBudgetInfo.Text = "Over/Under Budget";
+            // 
+            // labelAdd
+            // 
+            this.labelAdd.AutoSize = true;
+            this.labelAdd.Font = new System.Drawing.Font("Modern No. 20", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAdd.Location = new System.Drawing.Point(465, 474);
+            this.labelAdd.Name = "labelAdd";
+            this.labelAdd.Size = new System.Drawing.Size(167, 21);
+            this.labelAdd.TabIndex = 3;
+            this.labelAdd.Text = "Add New Payment";
             // 
             // BudgetForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 516);
+            this.ClientSize = new System.Drawing.Size(1162, 710);
+            this.Controls.Add(this.labelAdd);
+            this.Controls.Add(this.labelBudgetInfo);
+            this.Controls.Add(this.labelGroup);
+            this.Controls.Add(this.labelSingleItem);
             this.Name = "BudgetForm";
             this.Text = "BudgetTracking";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelSingleItem;
+        private System.Windows.Forms.Label labelGroup;
+        private System.Windows.Forms.Label labelBudgetInfo;
+        private System.Windows.Forms.Label labelAdd;
     }
 }
 
