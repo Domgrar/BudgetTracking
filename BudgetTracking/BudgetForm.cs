fc2cf5d6f7a2fa99d919310e40cb8d0ccb6d60b3
Jeremy Farmer <you.icaruspsy@gmail.com>
2018-04-14 13:13:04 -0400

    Add project files.

---
+++ BudgetTracking/BudgetForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BudgetTracking
+{
+    public partial class BudgetForm : Form
+    {
+        public BudgetForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
